94cd3ecf7eb15a9cb3b2c64fa658ed7768d7a9c2
neaxi <sustacek@centrum.cz>
2019-05-24 13:02:34 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# pins on which we have buttons for io.py
+pin_btn01 = 18
+pin_btn02 = 19
+
+
+# automatic wifi connection
+wifi_ssid = "random free wifi"
+wifi_passwd = "passwd"
+
+
+# M2X variables
+m2x_device_id = "device_id_123"
+m2x_api_key = "super_uber_secret_API_key"
